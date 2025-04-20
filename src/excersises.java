9d2965bd83b4696c48b5e6f14fe2728274df02f4
Stephen Wachira Maingi <wachira maingi>
2025-04-20 02:03:42 +0200

    Initial commit for astrophysics project

---
+++ src/excersises.java
@@ -0,0 +1,17 @@
+public class excersises {
+   public  static void main(String[] args){
+       // Shared data 
+       String lastName = "Maingi";
+       String cityOfBirth = "Nyahururu";
+       int yearOfBirth = 2010;
+
+       // Twin 1
+       String firstName1 = "Kimarū";
+       String email1 = "kimaru.maingi@spaceuniversity.com";
+
+       // Twin 2
+       String firstName2 = "Kinyanjui";
+       String email2 = "kinyanjui.maingi@spaceuniversity.com";
+       System.out.print();
+   }
+}
